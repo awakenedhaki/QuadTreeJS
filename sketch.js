a2bd1924545702bb14fbdbfea78c36ec2eb10167
awakenedhaki <rodrigo.vallejosch@gmail.com>
2023-11-26 17:53:44 -0800

    Visualization of query boundary

--- sketch.js
+++ sketch.js
@@ -1,15 +1,86 @@
-let qtree, point, query;
+// Declaring Global Variables ==================================================
+let qtree, retrievedPoints, query;
+let queryX1, queryX2, queryY1, queryY2;
 
+// Setting Constants ===========================================================
+const mintGreenRGB = [201, 237, 220];
+
+// p5js Boilerplate ============================================================
 function setup() {
   createCanvas(500, 500);
-  qtree = new QuadTree(new Rectangle(width / 2, height / 2, width, height), 2);
+
+  qtree = new QuadTree(new Rectangle(width / 2, height / 2, width, height), 4);
+
+  // Seeding QuadTree with random points
+  let points = [];
+  for (let i = 0; i < 250; i++) {
+    points.push(new Point(random(width), random(height)));
+  }
+  qtree.insertPoints(points);
 }
 
 function draw() {
   background(0);
   drawQuadTree(qtree);
+
+  // Querying QuadTree
+  query = constructQueryBoundary(queryX1, queryY1, queryX2, queryY2);
+  if (query !== null) {
+    drawBoundary(query, mintGreenRGB);
+    retrievedPoints = qtree.queryRange(query);
+    drawPoints(retrievedPoints, 10);
+  }
 }
 
+function mousePressed() {
+  queryX1 = mouseX;
+  queryY1 = mouseY;
+}
+
+function mouseDragged() {
+  queryX2 = mouseX;
+  queryY2 = mouseY;
+}
+
+function mouseReleased() {
+  queryX2 = mouseX;
+  queryY2 = mouseY;
+}
+
+// Helper Functions ============================================================
+/**
+ * Constructs a boundary rectangle object based on provided coordinates.
+ *
+ * @param {number} queryX1 - The x-coordinate of the first point.
+ * @param {number} queryY1 - The y-coordinate of the first point.
+ * @param {number} queryX2 - The x-coordinate of the second point.
+ * @param {number} queryY2 - The y-coordinate of the second point.
+ *
+ * @returns {Rectangle|null} Returns a Rectangle object representing the
+ * constructed boundary based on the provided coordinates. Returns null if any
+ * of the input values are not numbers.
+ *
+ * @description
+ * This function constructs a Rectangle object using the coordinates provided.
+ * It calculates the center point and dimensions of the rectangle based on the
+ * coordinates of two points. If any of the input values are not numbers, it
+ * returns null.
+ */
+function constructQueryBoundary(queryX1, queryY1, queryX2, queryY2) {
+  if (isNaN(queryX1) || isNaN(queryX2) || isNaN(queryY1) || isNaN(queryY2)) {
+    return null;
+  }
+
+  let centerX = (queryX2 + queryX1) / 2;
+  let centerY = (queryY2 + queryY1) / 2;
+
+  let width = abs(queryX2 - queryX1);
+  let height = abs(queryY2 - queryY1);
+
+  return new Rectangle(centerX, centerY, width, height);
+}
+
+// Drawing Custom Objects ======================================================
 function drawQuadTree(qtree) {
   if (qtree.children.length != 0) {
     qtree.children.forEach((child) => {
@@ -17,35 +88,33 @@ function drawQuadTree(qtree) {
     });
   }
 
-  drawBoundary(qtree.boundary);
+  drawBoundary(qtree.boundary, [255, 255, 255]);
   drawPoints(qtree.points);
 }
 
-function drawBoundary(boundary) {
+function drawBoundary(boundary, rgb) {
   let x = boundary.x;
   let y = boundary.y;
   let width = boundary.width;
   let height = boundary.height;
 
-  noFill();
-  stroke(255);
+  if (rgb.every((element) => element === 255)) {
+    noFill();
+  } else {
+    fill(...rgb, 40);
+  }
+  stroke(...rgb);
   rectMode(CENTER);
   rect(x, y, width, height);
 }
 
-function drawPoints(points) {
+function drawPoints(points, size) {
+  if (size === undefined) {
+    size = 2;
+  }
+
   fill(255);
   points.forEach((point) => {
-    ellipse(point.x, point.y, 2, 2);
+    ellipse(point.x, point.y, size, size);
   });
-}
-
-function mousePressed() {
-  let points = [];
-  for (let i = 0; i < 10; i++) {
-    points.push(new Point(mouseX + random(-10, 10), mouseY + random(-10, 10)));
-  }
-  qtree.insertPoints(points);
-  points = [];
-  console.log(qtree);
 }
